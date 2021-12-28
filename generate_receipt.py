976dacf78bc771dbfdcff2f4560cf1ae17333e5e
rfn900 <rfn900@gmail.com>
2021-12-29 00:18:18 +0100

    Generates multiple pdfs from api result

--- generate_receipt.py
+++ generate_receipt.py
@@ -1,11 +1,27 @@
 from jinja2 import Environment, FileSystemLoader
 import pdfkit
 import os
+import json
 import base64
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
+
+
+def suffix(d):
+    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+
+
+def get_formatted_date(unformatted_date):
+    dt = unformatted_date.split('T')[0].split('-')
+    date_string = datetime(int(dt[0]), int(dt[1]), int(dt[2])).strftime('%b {S}, %Y')
+    # Return date with right format and day suffix (1st, 2nd, 21st...)
+    return date_string.replace('{S}', str(int(dt[2])) + suffix(int(dt[2])))
+
+currency_dict = {'USD': '$', 'EUR': '€'}
+
 load_dotenv()
-headers = {
+access_token_headers = {
     'Accept': 'application/json',
     'Accept-Language': 'en_US',
 }
@@ -14,21 +30,39 @@ data = {
   'grant_type': 'client_credentials'
 }
 
+# Loading env variables...
 CLIENT_ID = os.environ.get('PAYPAL_CLIENT_ID')
 SECRET = os.environ.get('PAYPAL_SECRET')
+BASE_URL = os.environ.get('BASE_URL')
+PAYEE_COMPANY_NAME = os.environ.get('PAYEE_COMPANY_NAME')
+PAYEE_ADDRESS = os.environ.get('PAYEE_ADDRESS')
+PAYEE_TAX_ID = os.environ.get('PAYEE_TAX_ID')
+PAYEE_WEBSITE = os.environ.get('PAYEE_WEBSITE')
+PAYEE_EMAIL = os.environ.get('PAYEE_EMAIL')
+
 auth = (CLIENT_ID, SECRET)
 
-response = requests.post('https://api-m.sandbox.paypal.com/v1/oauth2/token',
-                         headers=headers, data=data, auth=auth)
+response = requests.post(f'{BASE_URL}/oauth2/token',
+                         headers=access_token_headers, data=data, auth=auth)
 
 access_token = response.json()['access_token']
 
-# TODO => Grab token and call transactions endpoint
+transaction_headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {access_token}',
+}
+start_date = input('Enter start date (format: YYYY-MM-DD): ')
+end_date = input('Enter end date (format: YYYY-MM-DD)): ')
 
-# TODO => Grab Json and pull information for filling up temmplate file
+
+res = requests.get(f'{BASE_URL}/reporting/transactions?start_date={start_date}T00:00:00-0700&end_date={end_date}T23:59:59-0700&fields=all&page_size=100&page=1&transaction_type=T0002',
+             headers=transaction_headers)
+
+transactions_array = res.json()['transaction_details']
+
 root = os.path.dirname(os.path.abspath(__file__))
 
-logo_filename = os.path.join(root, 'assets', 'yayloh-small.png')
+logo_filename = os.path.join(root, 'assets', 'logo.png')
 
 def get_image_file_as_base64_data():
     with open(logo_filename, 'rb') as image_file:
@@ -39,49 +73,84 @@ templates_dir = os.path.join(root, 'templates')
 env = Environment( loader = FileSystemLoader(templates_dir) )
 template = env.get_template('template.html')
 
-# receipts.html is a temporary name!
-# Should be called --transaction-id--.html (or something...)
-filename = os.path.join(root, 'receipts/HTML', 'receipts.html') 
+
+##################################################
+# This is only for testing
+# TODO -> Remove this test when running it...
+with open('transaction_details.json') as json_file:
+    transactions_array = json.load(json_file)['transaction_details']
+##################################################
+
+print(f'Found {len(transactions_array)} recepits in that range')
+for index, transaction in enumerate(transactions_array):
+    print(f'Printing now receipt # {index+1}')
+    transaction_info = transaction['transaction_info']
+
+    with open('transaction_details.json') as json_file:
+        payer_info = json.load(json_file)['transaction_details'][index]['payer_info']
 
 
-payer_address_line2 = ""
-payer_postal_code = 75003
-payer_city = "Paris"
-payer_country = "France"
-with open(filename, 'w') as fh:
-    fh.write(template.render(
-        image_base64 = get_image_file_as_base64_data().decode(),
-        title = "Receipt Company A",
-        paypal_reference = "I-G2349194919",
-        transaction_id = "9034199-4912",
-        invoice_date = "Nov 12th, 21",
-        payment_date = "Nov 12th, 21",
-        status = "PAID",
-        balance_due ="€0.00",
-        payer_name = "Company A",
-        payer_address_line1 = "Rue les bleu 21",
-        payer_address_line2 = f"{payer_postal_code} {payer_city}",
-        payer_country = payer_country,
-        total_price = "€297.00",
-        ammount_paid = "€297.00",
-        reference_name = "Julie Liot",
-        reference_email = "julie@smoon-lingerie.com",
-    ))
+    with open('payers_information.json') as payer_file:
+        payer_details = json.load(payer_file)
 
-# TODO => Uncomment that when done with the API calls
-# pdf_filename = os.path.join(root, 'receipts/PDF', 'receipts.pdf') 
 
-# options = {
-    # 'page-size': 'Letter',
-    # 'margin-top': '0.25in',
-    # 'margin-right': '0.3in',
-    # 'margin-bottom': '0.25in',
-    # 'margin-left': '0.5in',
-    # 'encoding': "UTF-8",
-    # 'custom-header': [
-        # ('Accept-Encoding', 'gzip')
-    # ],
-    # 'no-outline': None
-# }
+    paypal_reference_id =  transaction_info['paypal_reference_id']
+    transaction_id = transaction_info['transaction_id']
+    invoice_date = get_formatted_date(transaction_info['transaction_initiation_date'])
+    payment_date = get_formatted_date(transaction_info['transaction_updated_date'])
 
-# pdfkit.from_file(filename,pdf_filename, options=options) 
+    retailer = payer_details[payer_info['email_address']]
+
+    payer_name = retailer['company_name']
+    payer_address = retailer['street_address']
+    payer_reference_name = retailer['reference_name']
+    payer_reference_email = payer_info['email_address']
+    payment_amount = transaction_info['transaction_amount']['value']
+    payer_postal_code = retailer['postal_code']
+    payer_city = retailer['city']
+    payer_country = retailer['country']
+    currency_symbol = currency_dict[transaction_info['transaction_amount']['currency_code']]
+
+    filename = os.path.join(root, 'receipts/HTML', f'{paypal_reference_id}.html') 
+
+    with open(filename, 'w') as fh:
+        fh.write(template.render(
+            payee_company_name = PAYEE_COMPANY_NAME,
+            payee_address = PAYEE_ADDRESS,
+            payee_tax_id = PAYEE_TAX_ID,
+            payee_website = PAYEE_WEBSITE,
+            payee_email = PAYEE_EMAIL,
+            image_base64 = get_image_file_as_base64_data().decode(),
+            title = f"Receipt {payer_name}",
+            paypal_reference = paypal_reference_id,
+            transaction_id = transaction_id,
+            invoice_date = invoice_date,
+            payment_date = payment_date,
+            status = "PAID",
+            balance_due = f'{currency_symbol} 0.00',
+            payer_name = payer_name,
+            payer_address_line1 = payer_address,
+            payer_address_line2 = f"{payer_postal_code} {payer_city}",
+            payer_country = payer_country,
+            total_price = f"{currency_symbol} {payment_amount}",
+            ammount_paid = f"{currency_symbol} {payment_amount}",
+            reference_name = payer_reference_name,
+            reference_email = payer_reference_email,
+        ))
+
+    pdf_filename = os.path.join(root, 'receipts/PDF', f'{paypal_reference_id}.pdf') 
+
+    options = {
+        'page-size': 'Letter',
+        'margin-top': '0.25in',
+        'margin-right': '0.3in',
+        'margin-bottom': '0.25in',
+        'margin-left': '0.5in',
+        'encoding': "UTF-8",
+        'custom-header': [
+            ('Accept-Encoding', 'gzip')
+        ],
+        'no-outline': None
+    }
+
+    pdfkit.from_file(filename, pdf_filename, options=options) 
